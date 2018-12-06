212caf81517f1b9f68fa215877625a4843056e9b
António Manuel Vieira Ramadas <antonio_ramadas@hotmail.com>
2018-12-06 16:22:25 +0000

    Solve part 1 of day 6

--- day-00101/solution.cpp
+++ day-00101/solution.cpp
@@ -8,10 +8,14 @@
 
 using namespace std;
 
+int manhattamDistance(pair<int, int> lhs, pair<int, int> rhs) {
+    return abs(lhs.first - rhs.first) + abs(lhs.second - rhs.second);
+}
+
 void partOne() {
     int x, y, maxX = -1, maxY = -1;
     char ignore;
-    list<pair<int, int> > coordinates;
+    vector<pair<int, int> > coordinates;
 
     ifstream fin("input");
     while (fin >> x >> ignore >> y) {
@@ -21,11 +25,42 @@ void partOne() {
     }
     fin.close();
 
-    vector<vector<pair<int, int> > > grid(maxY, vector<pair<int, int> >(maxX, make_pair(-1, -1)));
+    int closestCoordinate = -1, coordinateDistance = -1;
+    pair<int, int> currentCoordinates;
+    vector<int> count(coordinates.size(), 0);
 
+    for (y = 0; y <= maxY; y++) {
+        for (x = 0; x <= maxX; x++) {
+            closestCoordinate = -1;
+            coordinateDistance = -1;
+            currentCoordinates = make_pair(x, y);
 
+            for (int i = 0; i < coordinates.size(); i++) {
+                int distance = manhattamDistance(currentCoordinates, coordinates.at(i));
 
-    cout << "Solution to part one = " << endl;
+                if (coordinateDistance == -1 || distance <= coordinateDistance) {
+                    if (distance == coordinateDistance && coordinateDistance != -1) {
+                        // Same distance to two coordinates
+                        closestCoordinate = -1;
+                    } else {
+                        closestCoordinate = i;
+                    }
+
+                    coordinateDistance = distance;
+                }
+            }
+
+            if (closestCoordinate != -1) {
+                if (x == 0 || y == 0 || x == maxX || y == maxY) {
+                    count.at(closestCoordinate) = -1;
+                } else if (count.at(closestCoordinate) != -1) {
+                    count.at(closestCoordinate)++;
+                }
+            }
+        }
+    }
+
+    cout << "Solution to part one = " << *max_element(count.begin(), count.end()) << endl;
 }
 
 int main() {
