35b0b3dbceb9045944adf140e102739449b4262a
António Ramadas <antonio_ramadas@hotmail.com>
2018-12-07 21:56:49 +0000

    Solve day 7 part 2

--- day-00110/solution.cpp
+++ day-00110/solution.cpp
@@ -7,19 +7,20 @@
 #include <list>
 #include <unordered_map>
 #include <queue>
+#include <set>
 
 using namespace std;
 
 void partOne() {
 
     string line;
-    char node[32], child[32];
-    unordered_map<string, pair<int, list<string> > > inDegree;
-    priority_queue<string, vector<string>, greater<> > candidates;
+    unordered_map<char, pair<int, list<char> > > inDegree;
+    priority_queue<char, vector<char>, greater<> > candidates;
 
     ifstream fin("input");
     while (getline(fin, line)) {
-        sscanf(line.c_str(), "Step %s must be finished before step %s can begin.", node, child);
+        char node, child;
+        sscanf(line.c_str(), "Step %c must be finished before step %c can begin.", &node, &child);
 
         inDegree[child].first++;
         inDegree[node].second.emplace_back(child);
@@ -32,7 +33,8 @@ void partOne() {
         }
     }
 
-    string solution = "", current;
+    string solution;
+    char current;
 
     while (!inDegree.empty() && !candidates.empty()) {
         current = candidates.top();
@@ -42,7 +44,7 @@ void partOne() {
         candidates.pop();
 
         for (const auto &child : inDegree[current].second) {
-            if (--inDegree[child].first <= 0) {
+            if (--inDegree[child].first == 0) {
                 candidates.push(child);
             }
         }
@@ -53,11 +55,73 @@ void partOne() {
     cout << "Solution to part one = " << solution << endl;
 }
 
+void partTwo() {
+    string line;
+    unordered_map<char, pair<int, list<char> > > inDegree;
+    set<pair<int, char> > readyToWorkOn;
+    set<pair<int, int> > active;
+
+    ifstream fin("input");
+    while (getline(fin, line)) {
+        char node, child;
+
+        sscanf(line.c_str(), "Step %c must be finished before step %c can begin.", &node, &child);
+
+        inDegree[child].first++;
+        inDegree[node].second.emplace_back(child);
+    }
+    fin.close();
+
+    // Get first jobs
+    for (const auto &it : inDegree) {
+        if (it.second.first == 0) {
+            readyToWorkOn.emplace(it.first - 'A' + 1 + 60, it.first);
+        }
+    }
+
+    int solution = 0;
+
+    while (!readyToWorkOn.empty() || !active.empty()) {
+        // Add work that is ready to the active workers
+        while (active.size() < 5 && !readyToWorkOn.empty()) {
+            active.insert(*readyToWorkOn.begin());
+            readyToWorkOn.erase(readyToWorkOn.begin());
+        }
+
+        // Get how much time should be advanced
+        int firstJobDone = active.begin()->first;
+        solution += firstJobDone;
+
+        // Advance time
+        set<pair<int, int> > activeTmp;
+        for (auto it : active) {
+            activeTmp.emplace(it.first - firstJobDone, it.second);
+        }
+        active = activeTmp;
+
+        // Get more jobs after finishing the current one(s)
+        while (!active.empty() && active.begin()->first <= 0) {
+            pair<int, char> current = *active.begin();
+            active.erase(active.begin());
+
+            for (const auto &child : inDegree[current.second].second) {
+                // If the previous nodes already went to the next step...
+                // (equals 0 (and not <=) prevents adding the same node multiple times)
+                if (--inDegree[child].first == 0) {
+                    readyToWorkOn.emplace(child - 'A' + 1 + 60, child);
+                }
+            }
+        }
+    }
+
+    cout << "Solution to part two = " << solution << endl;
+}
+
 int main() {
     cout << "--- The Sum of Its Parts ---" << endl;
 
     partOne();
-    // partTwo();
+    partTwo();
 
     return 0;
 }
