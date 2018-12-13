1058f72b34856f6cd9479e96a3e1117e54fc4993
António Ramadas <antonio_ramadas@hotmail.com>
2018-12-13 19:54:09 +0000

    Solve day 13 part 1

--- day-01100/solution.cpp
+++ day-01100/solution.cpp
@@ -14,6 +14,101 @@ private:
     Direction direction;
     Turn turn = Turn::LEFT;
 
+    void updateTurn() {
+        switch (turn) {
+            case Turn::LEFT:
+                turn = Turn::STRAIGHT;
+                break;
+            case Turn::STRAIGHT:
+                turn = Turn::RIGHT;
+                break;
+            case Turn::RIGHT:
+                turn = Turn::LEFT;
+                break;
+            default:
+                cerr << "Wrong turn!" << endl;
+                break;
+        }
+    }
+
+    void updateIntersectionDirection() {
+        switch (turn) {
+            case Turn::LEFT:
+                switch (direction) {
+                    case Direction::UP:
+                        direction = Direction::LEFT;
+                        break;
+                    case Direction::DOWN:
+                        direction = Direction::RIGHT;
+                        break;
+                    case Direction::LEFT:
+                        direction = Direction::DOWN;
+                        break;
+                    case Direction::RIGHT:
+                        direction = Direction::UP;
+                        break;
+                }
+                break;
+            case Turn::STRAIGHT:
+                break;
+            case Turn::RIGHT:
+                switch (direction) {
+                    case Direction::UP:
+                        direction = Direction::RIGHT;
+                        break;
+                    case Direction::DOWN:
+                        direction = Direction::LEFT;
+                        break;
+                    case Direction::LEFT:
+                        direction = Direction::UP;
+                        break;
+                    case Direction::RIGHT:
+                        direction = Direction::DOWN;
+                        break;
+                }
+                break;
+            default:
+                cerr << "Wrong turn!" << endl;
+                break;
+        }
+
+        updateTurn();
+    }
+
+    void updateFrontCurveDirection() {
+        switch (direction) {
+            case Direction::UP:
+                direction = Direction::RIGHT;
+                break;
+            case Direction::DOWN:
+                direction = Direction::LEFT;
+                break;
+            case Direction::LEFT:
+                direction = Direction::DOWN;
+                break;
+            case Direction::RIGHT:
+                direction = Direction::UP;
+                break;
+        }
+    }
+
+    void updateBackDirection() {
+        switch (direction) {
+            case Direction::UP:
+                direction = Direction::LEFT;
+                break;
+            case Direction::DOWN:
+                direction = Direction::RIGHT;
+                break;
+            case Direction::LEFT:
+                direction = Direction::UP;
+                break;
+            case Direction::RIGHT:
+                direction = Direction::DOWN;
+                break;
+        }
+    }
+
 public:
     Cart(int row, int col, Direction dir) {
         coordinates = make_pair(row, col);
@@ -26,6 +121,51 @@ public:
 
     pair<int, int> getCoordinates() const {
         return coordinates;
+    }
+
+    Direction getDirection() const {
+        return direction;
+    }
+
+    Turn getTurn() const {
+        return turn;
+    }
+
+    void updateDirection(const char &symbol) {
+        switch (symbol) {
+            case '+':
+                updateIntersectionDirection();
+                break;
+            case '/':
+                updateFrontCurveDirection();
+                break;
+            case '\\':
+                updateBackDirection();
+                break;
+            default:
+                break;
+        }
+    }
+
+    void move() {
+        pair<int, int> dir;
+
+        switch (direction) {
+            case Direction::UP:
+                dir = make_pair(-1, 0);
+                break;
+            case Direction::DOWN:
+                dir = make_pair(+1, 0);
+                break;
+            case Direction::LEFT:
+                dir = make_pair(0, -1);
+                break;
+            case Direction::RIGHT:
+                dir = make_pair(0, +1);
+                break;
+        }
+
+        coordinates = make_pair(coordinates.first + dir.first, coordinates.second + dir.second);
     }
 };
 
@@ -49,15 +189,15 @@ private:
     bool shouldBeIntersection(int row, int col) {
         bool allConditionsTrue = true;
 
-        allConditionsTrue &= row > 0;
-        allConditionsTrue &= row+1 < crop.size();
-        allConditionsTrue &= isVerticalConnection(row-1, col);
-        allConditionsTrue &= isVerticalConnection(row+1, col);
+        allConditionsTrue = allConditionsTrue && row > 0;
+        allConditionsTrue = allConditionsTrue && row+1 < crop.size();
+        allConditionsTrue = allConditionsTrue && isVerticalConnection(row-1, col);
+        allConditionsTrue = allConditionsTrue && isVerticalConnection(row+1, col);
 
-        allConditionsTrue &= col > 0;
-        allConditionsTrue &= col+1 < crop.at(row).size();
-        allConditionsTrue &= isHorizontalConnection(row, col-1);
-        allConditionsTrue &= isHorizontalConnection(row, col+1);
+        allConditionsTrue = allConditionsTrue && col > 0;
+        allConditionsTrue = allConditionsTrue && col+1 < crop.at(row).size();
+        allConditionsTrue = allConditionsTrue && isHorizontalConnection(row, col-1);
+        allConditionsTrue = allConditionsTrue && isHorizontalConnection(row, col+1);
 
         return allConditionsTrue;
     }
@@ -66,17 +206,17 @@ private:
         bool allConditionsTrue = true;
 
         if (row > 0) {
-            allConditionsTrue &= !isVerticalConnection(row-1, col);
+            allConditionsTrue = allConditionsTrue && !isVerticalConnection(row-1, col);
         }
 
         if (row+1 < crop.size()) {
-            allConditionsTrue &= !isVerticalConnection(row+1, col);
+            allConditionsTrue = allConditionsTrue && !isVerticalConnection(row+1, col);
         }
 
-        allConditionsTrue &= col > 0;
-        allConditionsTrue &= col+1 < crop.at(row).size();
-        allConditionsTrue &= isHorizontalConnection(row, col-1);
-        allConditionsTrue &= isHorizontalConnection(row, col+1);
+        allConditionsTrue = allConditionsTrue && col > 0;
+        allConditionsTrue = allConditionsTrue && col+1 < crop.at(row).size();
+        allConditionsTrue = allConditionsTrue && isHorizontalConnection(row, col-1);
+        allConditionsTrue = allConditionsTrue && isHorizontalConnection(row, col+1);
 
         return allConditionsTrue;
     }
@@ -84,17 +224,17 @@ private:
     bool shouldBeVerticalConnection(int row, int col) {
         bool allConditionsTrue = true;
 
-        allConditionsTrue &= row > 0;
-        allConditionsTrue &= row+1 < crop.size();
-        allConditionsTrue &= isVerticalConnection(row-1, col);
-        allConditionsTrue &= isVerticalConnection(row+1, col);
+        allConditionsTrue = allConditionsTrue && row > 0;
+        allConditionsTrue = allConditionsTrue && row+1 < crop.size();
+        allConditionsTrue = allConditionsTrue && isVerticalConnection(row-1, col);
+        allConditionsTrue = allConditionsTrue && isVerticalConnection(row+1, col);
 
         if (col > 0) {
-            allConditionsTrue &= !isHorizontalConnection(row, col-1);
+            allConditionsTrue = allConditionsTrue && !isHorizontalConnection(row, col-1);
         }
 
         if (col+1 < crop.at(row).size()) {
-            allConditionsTrue &= !isHorizontalConnection(row, col+1);
+            allConditionsTrue = allConditionsTrue && !isHorizontalConnection(row, col+1);
         }
 
         return allConditionsTrue;
@@ -103,24 +243,24 @@ private:
     bool shouldBeFrontCurve(int row, int col) {
         bool up = true, down = true;
 
-        up &= row > 0;
-        up &= col > 0;
-        up &= isVerticalConnection(row-1, col) && isHorizontalConnection(row, col-1);
+        up = up && row > 0;
+        up = up && col > 0;
+        up = up && isVerticalConnection(row-1, col) && isHorizontalConnection(row, col-1);
         if (row+1 < crop.size()) {
-            up &= !isVerticalConnection(row+1, col);
+            up = up && !isVerticalConnection(row+1, col);
         }
         if (col+1 < crop.at(row).size()) {
-            up &= !isHorizontalConnection(row, col+1);
+            up = up && !isHorizontalConnection(row, col+1);
         }
 
-        down &= row+1 < crop.size();
-        down &= col+1 < crop.at(row).size();
-        down &= isVerticalConnection(row+1, col) && isHorizontalConnection(row, col+1);
+        down = down && row+1 < crop.size();
+        down = down && col+1 < crop.at(row).size();
+        down = down && isVerticalConnection(row+1, col) && isHorizontalConnection(row, col+1);
         if (row > 0) {
-            down &= !isVerticalConnection(row-1, col);
+            down = down && !isVerticalConnection(row-1, col);
         }
         if (col > 0) {
-            up &= !isHorizontalConnection(row, col-1);
+            down = down && !isHorizontalConnection(row, col-1);
         }
 
         return up || down;
@@ -129,24 +269,24 @@ private:
     bool shouldBeBackCurve(int row, int col) {
         bool up = true, down = true;
 
-        up &= row > 0;
-        up &= col+1 < crop.at(row).size();
-        up &= isVerticalConnection(row-1, col) && isHorizontalConnection(row, col+1);
+        up = up && row > 0;
+        up = up && col+1 < crop.at(row).size();
+        up = up && isVerticalConnection(row-1, col) && isHorizontalConnection(row, col+1);
         if (row+1 < crop.size()) {
-            up &= !isVerticalConnection(row+1, col);
+            up = up && !isVerticalConnection(row+1, col);
         }
         if (col > 0) {
-            up &= !isHorizontalConnection(row, col-1);
+            up = up && !isHorizontalConnection(row, col-1);
         }
 
-        down &= col > 0;
-        down &= row+1 < crop.size();
-        down &= isVerticalConnection(row+1, col) && isHorizontalConnection(row, col-1);
+        down = down && col > 0;
+        down = down && row+1 < crop.size();
+        down = down && isVerticalConnection(row+1, col) && isHorizontalConnection(row, col-1);
         if (row > 0) {
-            up &= !isVerticalConnection(row-1, col);
+            down = down && !isVerticalConnection(row-1, col);
         }
         if (col < crop.at(row).size()) {
-            up &= !isHorizontalConnection(row, col+1);
+            down = down && !isHorizontalConnection(row, col+1);
         }
 
         return up || down;
@@ -206,6 +346,33 @@ public:
             crop.at(row).at(col) = symbol;
         }
     }
+
+    bool tick(pair<int, int> &crashCoordinates) {
+        set<Cart> tmpCarts = carts;
+        set<Cart> newCarts;
+
+        for (const Cart &cart : carts) {
+            Cart newCart = cart;
+            pair<int, int> cartCoordinates = newCart.getCoordinates();
+
+            newCart.updateDirection(crop.at(cartCoordinates.first).at(cartCoordinates.second));
+
+            newCart.move();
+
+            tmpCarts.erase(cart);
+
+            // If it crashes with any of the previous ones or the ones that already moved...
+            if (tmpCarts.find(newCart) != tmpCarts.end() || newCarts.find(newCart) != newCarts.end()) {
+                crashCoordinates = newCart.getCoordinates();
+                return false;
+            }
+
+            newCarts.insert(newCart);
+        }
+
+        carts = newCarts;
+        return true;
+    }
 };
 
 void partOne() {
@@ -221,7 +388,11 @@ void partOne() {
 
     crop.cleanCrop();
 
-    cout << "Solution to part one = " << endl;
+    pair<int, int> crashCoordinates;
+
+    while (crop.tick(crashCoordinates));
+
+    cout << "Solution to part one = " << crashCoordinates.second << "," << crashCoordinates.first << endl;
 }
 
 int main() {
